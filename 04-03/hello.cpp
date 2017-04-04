e8847a9893e773f744403b12d64626e070c4f1d9
Diba Mirza <dimirza@cs.ucsb.edu>
2017-04-03 18:07:39 -0700

    added new lecture

---
+++ 04-03/hello.cpp
@@ -0,0 +1,16 @@
+// hello.cpp
+// Diba Mirza
+// Prints hello to output
+
+#include <iostream>
+
+using namespace std; // Communicate with std i/o
+
+int main(){
+    int num = 16; //Declaring the variable num
+    cout<<"Enter a course number";
+    cin>>num;
+    cout << "Hello CS "<< num << " students!\n";
+    return 0;
+}
+
